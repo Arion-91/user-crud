4d2ab287b709c13cede97ecb55d25d423222d344
Arion <arion2107@gmail.com>
2019-10-15 11:52:00 +0600

    add locale storage

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,16 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+const USE_LOCALE_STORAGE = process.env.USE_LOCALE_STORAGE == 1;
+const storageKey = 'users';
+
+function updateLocaleStorage(data) {
+	localStorage.setItem(storageKey, JSON.parse(data))
+}
+
 let store = new Vuex.Store({
 	state: {
-		users: [],
+		users: USE_LOCALE_STORAGE && JSON.stringify(localStorage.getItem(storageKey)) || [],
 	},
 
 	mutations: {
@@ -16,15 +23,18 @@ let store = new Vuex.Store({
 			state.users.add({
 				id: lastId++,
 				name
-			})
+			});
+			updateLocaleStorage(state.users);
 		},
 		updateUser(state, payload) {
 			let userIndex = state.users.findIndex(user => user.id === payload.id);
 			if (userIndex < 0) return;
 			state.users[userIndex].name = payload.name;
+			updateLocaleStorage(state.users);
 		},
 		deleteUser(state, id) {
 			state.users = state.users.filter(user => user.id === id);
+			updateLocaleStorage(state.users);
 		}
 	},
 });

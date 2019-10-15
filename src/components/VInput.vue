7eaec804463a64ddd3a658d8f14c1fbcc9b1acca
Arion <arion2107@gmail.com>
2019-10-15 19:19:38 +0600

    add wrapper for input

--- src/components/VInput.vue
+++ src/components/VInput.vue
@@ -1,18 +1,26 @@
 <template>
-	<input :id="id" type="text" :placeholder="label" :value="value" @change="func">
+	<div class="wrap-list">
+		<input :id="id" type="text" :placeholder="label" :value="value" @change="func">
+	</div>
 </template>
 
 <script>
 	export default {
 		name: "SearchInput",
 		props: ['id', 'label', 'value', 'func'],
-		methods: {
-
-		}
 	}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+	.wrap-list {
+		width: 100%;
+		padding: 6px 3px;
+
+		@media (max-width: 768px) {
+			padding: 3px;
+		}
+	}
+
 	input {
 		display: block;
 		padding: 6px 12px;
@@ -22,11 +30,5 @@
 		font-weight: 400;
 		border: 1px solid #ced4da;
 		overflow: visible;
-	}
-
-	@media (max-width: 768px) {
-		input {
-			margin: 3px;
-		}
 	}
 </style>

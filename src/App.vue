66d7ef0328e744812fd095e1545f8f5d8232669b
Arion <arion2107@gmail.com>
2019-10-15 13:16:25 +0600

    add List

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,32 @@
 <template>
 	<div id="app">
+		<List/>
 	</div>
 </template>
 
 <script>
+	import List from "@/components/List";
+
 	export default {
 		name: 'app',
-		components: {}
+		components: {
+			List
+		}
 	}
 </script>
 
 <style>
+	* {
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+	}
+
 	#app {
 		font-family: 'Avenir', Helvetica, Arial, sans-serif;
 		-webkit-font-smoothing: antialiased;
 		-moz-osx-font-smoothing: grayscale;
 		text-align: center;
 		color: #2c3e50;
-		margin-top: 60px;
 	}
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,58 @@
+<template>
+	<table class="wrap">
+		<tr class="head">
+			<th>ID</th>
+			<th>Наименование</th>
+		</tr>
+		<tr v-for="user in users" :key="user.id" class="item">
+			<td class="item-id">{{user.id}}</td>
+			<td class="item-name">{{user.name}}</td>
+		</tr>
+	</table>
+</template>
+
+<script>
+	export default {
+		name: "List",
+		computed: {
+			users() {
+				return this.$store.state.users;
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.wrap {
+		width: 100%;
+	}
+
+	table {
+		border-collapse: collapse;
+	}
+	th, td {
+		padding: 12px;
+	}
+
+	.head {
+		background-color: #343a40;
+		border: #454d55;
+		border-bottom: 2px solid #dee2e6;
+		color: white;
+	}
+
+	.item {
+		&-id {
+			font-weight: 600;
+		}
+		&-name {
+			text-align: left;
+		}
+	}
+
+	.item:hover {
+		color: #495057;
+		background-color: #e9ecef;
+		border-color: #dee2e6;
+	}
+</style>
